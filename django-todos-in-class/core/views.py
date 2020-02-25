784e6d677dd1a79901266a1e43688b6229783e78
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-02-24 21:05:22 -0500

    simple html that renders todos from data.py

--- django-todos-in-class/core/views.py
+++ django-todos-in-class/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import data
+
 # Create your views here.
 def index(request):
-    return HttpResponse("This is just a text response, unrelated to the database")
+    todos = data.TODOS
+    breakpoint()
+    return render(request, 'base.html', {'todos': todos})
---
+++ django-todos-in-class/data.py
@@ -0,0 +1,4 @@
+TODOS = {'1': {'title': 'Mow the yard', 'details': 'Do this by the weekend'},
+        '2': {'title': 'Wash the Car', 'details': 'do this before your trip to the ER'} 
+}
+
