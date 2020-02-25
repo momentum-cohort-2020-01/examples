1bf7f0f488b28f78881858e0fd152f878579be9a
Amy Gori <amydgori@gmail.com>
2020-02-25 13:25:30 -0500

    Make a change to a model and make a new migration

--- django-todos-in-class/core/models.py
+++ django-todos-in-class/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     item = models.CharField(max_length=80)
-    description = models.TextField(max_length=280)
+    description = models.TextField(max_length=300)
 
     def __str__(self):
         return f"Todo item: {self.item} description: {self.description}"
