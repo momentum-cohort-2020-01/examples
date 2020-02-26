b0e08829187c06869ee1924a5fb120f7f2762058
Amy Gori <amydgori@gmail.com>
2020-02-26 16:47:57 -0500

    Add created_at and updated_at to Todo

--- django-todos-in-class/core/models.py
+++ django-todos-in-class/core/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Todo(models.Model):
     item = models.CharField(max_length=80)
     description = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Todo item: {self.item} description: {self.description}"
