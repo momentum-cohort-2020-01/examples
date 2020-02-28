6f64884c4d1cca450cd205c277a5f425c9ddd96e
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-02-28 12:27:45 -0500

    add tags with slugs

--- django-todos-in-class/core/views.py
+++ django-todos-in-class/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Todo
@@ -6,7 +6,7 @@ from .forms import TodoForm
 
 
 def todos_list(request):
-    todos = Todo.objects.all()
+    todos = Todo.objects.order_by('-created_at')
     return render(request, 'core/todos_list.html', {"todos": todos})
 
 
@@ -25,3 +25,20 @@ def todos_new(request):
         form = TodoForm()
 
     return render(request, 'core/todos_new.html', {"form": form})
+
+def todos_edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todos-detail', pk=todo.pk)
+    else:
+        form = TodoForm(instance=todo)
+    
+    return render(request, 'core/todos_edit.html',{"form": form})
+
+def todos_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect('todos-list')
