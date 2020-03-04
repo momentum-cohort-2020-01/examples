ac3e128c84de9ea08645606a3abb369d78696f8e
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-03-04 14:46:48 -0500

    intentional errors for debugging practice

--- django-voting-app/core/views.py
+++ django-voting-app/core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Cow, User, Vote
+from .models import User, Vote
 
 def index(request):
     cows = Cow.objects.all()
-    return render(request, "core/cows_list.html", {"cows": cows})
+    return render(request, "core/cows_list.html")
 
 
 
