555b4902b38cb1fee4fad3a8841203dec95aa921
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-03-05 11:47:55 -0500

    add capacity to vote for cow

--- django-voting-app/config/urls.py
+++ django-voting-app/config/urls.py
@@ -22,6 +22,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="home"),
+    path('vote/<int:cow_pk>', views.vote, name='vote'),
     path('accounts/', include('registration.backends.default.urls'))
 ]
 
